6efc87f3a9c5becc22ba954ead4d0740b2dcf9ea
Rob Kaiser <rob.kaiser22@gmail.com>
2017-10-04 18:00:57 -0400

    first commit

---
+++ src/amok/VirtualPetShelter.java
@@ -0,0 +1,5 @@
+package amok;
+
+public class VirtualPetShelter {
+
+}
---
+++ src/amok/VirtualPetShelterApp.java
@@ -0,0 +1,9 @@
+package amok;
+
+public class VirtualPetShelterApp {
+
+	public static void main(String[] args) {
+		System.out.println("just getting rid of error on git");
+	}
+
+}
